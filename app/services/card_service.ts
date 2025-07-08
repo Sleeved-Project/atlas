c00ed12a7d81b42ca618b4f49a687b67ad6da402
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-07-08 10:17:47 +0200

    merge(develop): resolve conflict

--- app/services/card_service.ts
+++ app/services/card_service.ts
@@ -78,34 +78,6 @@ export default class CardService {
       )
   }
 
-  public async getAllCardsBySetIdAndPaginate(
-    filters: Infer<typeof getAllCardsFiltersValidator>,
-    setId: string
-  ): Promise<ModelPaginatorContract<Card>> {
-    return await Card.query()
-      .join('Set', 'Card.set_id', 'Set.id')
-      .select('Card.id', 'Card.image_small')
-      .where('Card.set_id', setId)
-      .if(filters.name, (query) => query.whereILike('Card.name', `%${filters.name}%`))
-      .orderBy(
-        db.raw('CAST(NULLIF(REGEXP_REPLACE(Card.number, "[^0-9]", ""), "") AS UNSIGNED)'),
-        'asc'
-      )
-      .paginate(filters.page, filters.limit)
-  }
-
-  public async getAllCardsBySetId(setId: string): Promise<Card[]> {
-    return await Card.query()
-      .join('Set', 'Card.set_id', 'Set.id')
-      .select('Card.id', 'Card.image_small')
-      .where('Card.set_id', setId)
-      .orderBy('Set.release_date', 'asc')
-      .orderBy(
-        db.raw('CAST(NULLIF(REGEXP_REPLACE(Card.number, "[^0-9]", ""), "") AS UNSIGNED)'),
-        'asc'
-      )
-  }
-
   public async getCardIdById(id: string): Promise<Card> {
     return await Card.query().select('id').where('id', id).firstOrFail()
   }
