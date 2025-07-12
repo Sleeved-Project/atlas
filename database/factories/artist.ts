45b0bc46af576567060478ab86ee927250d545f8
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-07-12 16:03:25 +0200

    fix(test): fixed the artist pagination factory test error

--- database/factories/artist.ts
+++ database/factories/artist.ts
@@ -10,7 +10,6 @@ export const ArtistFactory = Factory.define(Artist, () => {
 
 export const PaginatedArtistFactory = Factory.define(Artist, ({ faker }) => {
   return {
-    id: faker.number.int({ min: 2, max: 1000 }),
     name: faker.person.fullName(),
   }
 }).build()
