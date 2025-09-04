c2629d44963d42a3c02df9f3a6ab97bd08cc1ceb
samuelaudic <samuelaudic@gmail.com>
2025-09-04 23:33:46 +0200

    refactor: rename UsersSearchService to UsersService in UsersController

--- app/controllers/product/users_controller.ts
+++ app/controllers/product/users_controller.ts
@@ -1,16 +1,16 @@
 import NotFoundException from '#exceptions/not_found_exception'
-import UsersSearchService from '#services/users_service'
+import UsersService from '#services/users_service'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 import { errors as lucidErrors } from '@adonisjs/lucid'
 
 @inject()
 export default class UsersController {
-  constructor(private usersSearchService: UsersSearchService) {}
+  constructor(private usersService: UsersService) {}
 
   async search({ request, response }: HttpContext) {
     try {
-      const users = await this.usersSearchService.searchUsers(request.qs())
+      const users = await this.usersService.searchUsers(request.qs())
       return response.ok(users)
     } catch (error) {
       if (error instanceof lucidErrors.E_ROW_NOT_FOUND) {
@@ -22,7 +22,7 @@ export default class UsersController {
 
   async show({ response, authUser }: HttpContext) {
     try {
-      const user = await this.usersSearchService.getUserById(authUser.id)
+      const user = await this.usersService.getUserById(authUser.id)
       return response.ok(user)
     } catch (error) {
       if (error instanceof lucidErrors.E_ROW_NOT_FOUND) {
