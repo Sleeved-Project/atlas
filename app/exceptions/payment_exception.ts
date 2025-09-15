9b016ad1fd19d163b58d1d157e410434a5b83499
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-09-15 22:46:52 +0200

    fix(stripe-webhook): now using the correct env key .......

--- app/exceptions/payment_exception.ts
+++ app/exceptions/payment_exception.ts
@@ -9,7 +9,7 @@ export class StripeException extends Exception {
   }
 
   async handle(error: this, ctx: HttpContext) {
-    console.error(error)
+    console.error('Stripe specific error : ', error)
     ctx.response.status(error.status).send({
       code: error.code,
       message: error.message,
