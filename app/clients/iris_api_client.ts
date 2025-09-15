3f5d27e317320a3e95ebcdfc555e5ce0c5f0c91c
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-09-15 10:37:13 +0200

    refactor(grading): refactorise grading input dto

--- app/clients/iris_api_client.ts
+++ app/clients/iris_api_client.ts
@@ -48,7 +48,7 @@ export default class IrisApiClient {
 
       const data = (await response.json()) as ScanGradeIrisResponse
 
-      if (!data.cards || data.cards.length === 0) {
+      if (!data) {
         throw new IrisNoMatchException()
       }
 
